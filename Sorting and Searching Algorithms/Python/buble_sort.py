25c0812812a2f0f4696b45be730d47c4f47443a6
David Musau <davie23musa@gmail.com>
2024-07-08 17:53:57 +0300

    Bubble Sort optimized

--- Sorting and Searching Algorithms/Python/buble_sort.py
+++ Sorting and Searching Algorithms/Python/buble_sort.py
@@ -1,10 +1,13 @@
-# Bubble sort in Python
+# Optimized Bubble sort in Python
 
 def bubbleSort(array):
     
-  # loop to access each array element
+  # loop through each element of array
   for i in range(len(array)):
-
+        
+    # keep track of swapping
+    swapped = False
+    
     # loop to compare array elements
     for j in range(0, len(array) - i - 1):
 
@@ -12,12 +15,18 @@ def bubbleSort(array):
       # change > to < to sort in descending order
       if array[j] > array[j + 1]:
 
-        # swapping elements if elements
+        # swapping occurs if elements
         # are not in the intended order
         temp = array[j]
         array[j] = array[j+1]
         array[j+1] = temp
 
+        swapped = True
+          
+    # no swapping means the array is already sorted
+    # so no need for further comparison
+    if not swapped:
+      break
 
 data = [-2, 45, 0, 11, -9]
 
